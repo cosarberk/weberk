1bd0c18017aa2aae2e379d8ba09ec07570000bd0
berkcosar <lookmainpoint@gmail.com>
2018-11-07 22:27:23 +0300

    firstcommit

---
+++ weberk.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import webbrowser
+
+
+print"Weberk : Websites Of Shortcut \n" "Developer : Berk Coşar"
+
+print """ 
+###########################################################
+   write to  websites code on list  for open the website
+###########################################################  
+    info: [i]-Websites name ==> write "i" on console
+   ___________________________________________________ 
+
+ 1 -Google             11 -W3schools   
+ 2 -Zoomeye            12 -Github
+ 3 -Duckduckgo         13 -Gitlab
+ 4 -Gmail              14 -Kali
+ 5 -Google Translate   15 -G.Play Console
+ 6 -Twitter            16 -G.Admob  
+ 7 -Instagram          17 -G.Adsence
+ 8 -Youtube            18 -Alastry(Hosting)
+ 9 -Unsplash 
+ 10 -Codepen 
+
+#################################################
+
+"""
+
+a="https://www.google.com/"
+b="https://www.zoomeye.org/"
+c="https://duckduckgo.com/"
+d="https://mail.google.com/mail/#inbox"
+e="https://translate.google.com/?hl=tr"
+f="https://twitter.com/"
+g="https://www.instagram.com/"
+h="https://www.youtube.com/"
+i="https://unsplash.com/"
+j="https://codepen.io/"
+k="https://www.w3schools.com/"
+l="https://github.com/"
+m="https://about.gitlab.com/"
+n="https://www.kali.org/"
+o="https://developer.android.com/distribute/console/"
+p="https://apps.admob.com"
+r="https://www.google.com/adsense/start/#"
+s="https://www.alastyr.com/"
+
+webcode = input("Please Write Websites code:")
+
+
+if webcode==1: webbrowser.open(a,new=2);
+if webcode==2: webbrowser.open(b,new=2);
+if webcode==3: webbrowser.open(c,new=2);
+if webcode==4: webbrowser.open(d,new=2);
+if webcode==5: webbrowser.open(e,new=2);
+if webcode==6: webbrowser.open(f,new=2);
+if webcode==7: webbrowser.open(g,new=2);
+if webcode==8: webbrowser.open(h,new=2);
+if webcode==9: webbrowser.open(i,new=2);
+if webcode==10: webbrowser.open(j,new=2);
+if webcode==11: webbrowser.open(k,new=2);
+if webcode==12: webbrowser.open(l,new=2);
+if webcode==13: webbrowser.open(m,new=2);
+if webcode==14: webbrowser.open(n,new=2);
+if webcode==16: webbrowser.open(o,new=2);
+if webcode==16: webbrowser.open(p,new=2);
+if webcode==17: webbrowser.open(r,new=2);
+if webcode==18: webbrowser.open(s,new=2);
+
+
+
+
+
+
+
+
+
+
